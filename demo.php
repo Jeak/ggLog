44e6b96a366c6fad843de86c3cd3ef6b7325ac9b
davidberard98 <david@kooia.info>
2013-08-28 17:20:19 -0700

    Javascript automatically fills in the date boxes

--- demo.php
+++ demo.php
@@ -10,7 +10,7 @@
     <script type="text/javascript" src="ggLogEssentials.js"></script>
     <script type="text/javascript" src="demo.js"></script>
   </head>
-  <body style="width:100%;">
+  <body style="width:100%;" onLoad="SetDateDropdown('datesdrop')">
     <?php require_once("navbar.php"); navbar("demo.php"); ?>
     <?php
 /*    $dbhandle = sqlite_open("data/user_test.db", 0666, $error);
@@ -83,60 +83,7 @@
           <p class="text-center"><b><span style="color:red">New</span> <span id="workoutname">Untitled Workout</span></b></p>
           <div style="position:relative;height:35px;width:100%;">
             <div style="position:absolute;top:0;left:0;">Title: <input type="text" name="title" id="ggLogwn" value="" oninput="changewn()" /></div>
-            <div style="position:absolute;top:0;right:0;">
-              Date:
-              <select name="month" style="width:70px;">
-                <option value="1">Jan</option>
-                <option value="2">Feb</option>
-                <option value="3">Mar</option>
-                <option value="4">Apr</option>
-                <option value="5">May</option>
-                <option value="6">Jun</option>
-                <option value="7">Jul</option>
-                <option value="8">Aug</option>
-                <option value="9">Sep</option>
-                <option value="10">Oct</option>
-                <option value="11">Nov</option>
-                <option value="12">Dec</option>
-              </select>
-              <select name="day" style="width:60px;">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
-              </select>
-              <select name="year" style="width:70px">
-                <option value="2013" selected>2013</option>
-                <option value="2012">2012</option>
-                <option value="2011">2011</option>
-              </select>
+            <div style="position:absolute;top:0;right:0;" id="datesdrop">
             </div>
           </div>
           <div style="position:relative;width:100%;height:170px;top:0">
@@ -211,6 +158,11 @@
           else return "0:00";
         }
         
+        function htmlnewline($in)
+        {
+          return str_replace("\n", "<br />\n", $in);
+        }
+        
         $preface="      ";
         $dbhandle = sqlite_open("data/user_test.db", 0666, $error);
         if (!$dbhandle) die ($error);
@@ -234,7 +186,7 @@
           echo "$preface  <div style=\"position:relative;top:0;left:0;width:100%;\">\n";
           
           echo "$preface    <div style=\"float:left;width:500px;margin-bottom:25px;\">";
-          echo stripslashes($data[$i][4]); // notes?
+          echo htmlnewline(stripslashes($data[$i][4])); // notes?
           echo "</div>\n";
           echo "$preface  </div>\n";
 
--- ggLogEssentials.js
+++ ggLogEssentials.js
@@ -6,17 +6,41 @@ function IsMobileBrowser()
 }
 function SetDateDropdown(idname)
 {
-  var contents ="Date: <select name=\"month\" style=\"width:70px;\">  <option value=\"1\">Jan</option>  <option value=\"2\">Feb</option>  <option value=\"3\">Mar</option>  <option value=\"4\">Apr</option>  <option value=\"5\">May</option>  <option value=\"6\">Jun</option>  <option value=\"7\">Jul</option>  <option value=\"8\">Aug</option>  <option value=\"9\">Sep</option>  <option value=\"10\">Oct</option>  <option value=\"11\">Nov</option>  <option value=\"12\">Dec</option></select> ";
+  var today = new Date();
+  var year = today.getUTCFullYear();
+  var date = today.getDate();
+  var month = today.getMonth();
+  var months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+  var contents ="Date: <select name=\"month\" style=\"width:70px;\"> ";
+  for(var i=0;i<12;++i)
+  {
+    if(i == month)
+    {
+      contents += "<option value=\"" + i+1 + "\" selected>" + months[i] + "</option>";
+    }
+    else
+    {
+      contents += "<option value=\"" + i+1 + "\">" + months[i] + "</option>";
+    }
+  }
+  
+//  var contents ="Date: <select name=\"month\" style=\"width:70px;\">  <option value=\"1\">Jan</option>  <option value=\"2\">Feb</option>  <option value=\"3\">Mar</option>  <option value=\"4\">Apr</option>  <option value=\"5\">May</option>  <option value=\"6\">Jun</option>  <option value=\"7\">Jul</option>  <option value=\"8\">Aug</option>  <option value=\"9\">Sep</option>  <option value=\"10\">Oct</option>  <option value=\"11\">Nov</option>  <option value=\"12\">Dec</option></select> ";
+  contents += "</select>"
   contents += "<select name=\"day\" style=\"width:60px;\">";
   for(var i=1;i<32;i++)
   {
-    contents += "<option value=\"" + i + "\">" + i + "</option>";
+    if(i == date)
+    {
+      contents += "<option value=\"" + i + "\" selected>" + i + "</option>";
+    }
+    else
+    {
+      contents += "<option value=\"" + i + "\">" + i + "</option>";
+    }
   }
   contents += "</select> ";
-  var today = new Date();
-  var year = today.getUTCFullYear();
   contents += "<select name=\"year\" style=\"width:70px\">"
-  for(var i=-2;i<3;i++)
+  for(var i=-2;i<1;i++)
   {
     if(i==0)
     {
