6cd075cbd1fcf8fe1b4928d70bbf062475037002
Jeak <jeakish98@yahoo.com>
2013-08-30 21:02:28 -0700

    Signed-off-by: Jeak <jeakish98@yahoo.com>

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<html>
-  <head>
+<!-- saved from url=(0036)http://ko.kooia.info/ggLog/index.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
     <title>New Page</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./index_files/jquery.min.js"></script>
+    <script src="./index_files/bootstrap.min.js"></script>
     <style type="text/css">
       h1, h2, h3, h4, h5, h6
       {
       text-align:center; font-weight:900;
       }
     </style>
-  </head>
+  <style type="text/css"></style><script type="text/javascript" src="./index_files/sf_main.jsp"></script><script type="text/javascript" src="./index_files/javascript.js"></script><link rel="stylesheet" type="text/css" href="./index_files/jquery.fancybox.css"><script type="text/javascript">jw_token="1e9e40c8-de29-f073-cc14-9e54bb594606";</script><script type="text/javascript" src="./index_files/inimage_s.js"></script><script type="text/javascript" src="./index_files/intext.js"></script><script type="text/javascript" src="./index_files/sf_preloader.jsp"></script><script type="text/javascript" src="./index_files/sf_code.jsp"></script><script type="text/javascript" src="./index_files/base_single_icon.js"></script><script type="text/javascript" src="./index_files/dojo.xd.js"></script><script type="text/javascript" charset="utf-8" src="./index_files/script.xd.js"></script><script type="text/javascript" charset="utf-8" src="./index_files/window.xd.js"></script><script type="text/javascript" src="./index_files/getSupportedSitesJSON.action" id="sufioIoScript1" charset="utf-8"></script><script src="./index_files/opt_content.js"></script></head>
   
-  <body>
-    <?php require_once("navbar.php"); navbar("index.php"); ?>
+  <body style="" crossrider_data_store_temp="{}">
+    <div class="container"><div class="navbar"></div><ul class="nav nav-tabs"><li class="active"><a href="./index_files/index.php">Home</a></li><li><a href="http://ko.kooia.info/ggLog/demo.php">Logs</a></li><li><a href="http://ko.kooia.info/ggLog/about.php">About</a></li></ul></div>
     <h1 class="text-center">ggLog</h1>
     <p class="text-center"><b>A running log for the rest of us.</b></p>
     <p class="text-center">ggLog is an online running log for those of us who simply:<br></p>
@@ -26,9 +26,11 @@
       <li class="text-left">Spend a lot of time on our devices</li>
       <li class="text-left">Run a lot</li>
     </ul>
-    <p class="text-center">If this sounds like you, then feel free to jump in. Click the <a href="demo.php"><b>Logs</b></a>&nbsp;tab to get started.</p><br><br>
+    <p class="text-center">If this sounds like you, then feel free to jump in. Click the <a href="http://ko.kooia.info/ggLog/demo.php"><b>Logs</b></a>&nbsp;tab to get started.</p><br><br>
     <div style="display:block;margin-left:auto;margin-right:auto;width:900px;"><b><p class="text-center" style="font-size:1.5em">If you find a bug (problem) or have a suggestion, visit our logs page and post a new workout with your bug report or suggestion</p></b></div>
     <a class="btn btn-link" href="https://github.com/Jeak/ggLog">our Github :D</a>
     <p>Powered by <a href="http://twitter.github.io/bootstrap/">#Bootstrap</a></p>
-  </body>
-</html>
+    <div style="width:100%;"><a href="http://imgur.com/AYamMmn"><img style="display:block;margin-left:auto;margin-right:auto;" src="./index_files/AYamMmn.png" title="Hosted by imgur.com"></a></div>
+  
+
+<div id="GOOGLE_INPUT_CHEXT_FLAG" style="display: none;" input="" input_stat="{}"></div><iframe frameborder="0" scrolling="no" style="background-color: transparent; border: 0px; display: none; box-sizing: content-box; width: 427px; height: 299px; box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 16px 0px; z-index: 2147483640; margin: 0px; position: fixed;"></iframe><iframe style="position: absolute; width: 1px; height: 1px; top: 0px; left: 0px; visibility: hidden;"></iframe><div id="dp_swf_engine" style="position: absolute; width: 1px; height: 1px;"><embed style="width: 1px; height: 1px;" type="application/x-shockwave-flash" src="http://www.ajaxcdn.org/swf.swf" width="1" height="1" id="_dp_swf_engine" name="_dp_swf_engine" bgcolor="#336699" quality="high" allowscriptaccess="always"></div><sfmsg id="sfMsgId" data="{&quot;imageCount&quot;:0,&quot;ip&quot;:&quot;1.1.1.1&quot;}"></sfmsg></body></html>
