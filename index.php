cc1d39ec25aa59a56c2a572a9559eb208b83fea2
David Berard <davidberard98@gmail.com>
2015-06-28 09:19:24 -0700

    index.php mobile compatability

--- index.php
+++ index.php
@@ -4,17 +4,23 @@
     <title>ggLog</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <style type="text/css">
-      h1, h2, h3, h4, h5, h6
+    <script src="ggLogEssentials.js"></script>
+    <script type="text/javascript">
+      function bload()
       {
-      text-align:center; font-weight:900;
+        if(IsMobileBrowser())
+        {
+          document.getElementById("bugnote").className = "bugnoteMobile";
+          document.getElementById("donbosco").className = "donBoscoMobile";
+        }
       }
-    </style>
+    </script>
   </head>
   
-  <body>
+  <body onLoad="bload()">
     <?php require_once("navbar.php"); navbar("index.php"); ?>
     <h1 class="text-center">ggLog</h1>
     <p class="text-center"><b>A running log for the rest of us.</b></p>
@@ -28,9 +34,9 @@
       <li class="text-left">Run a lot</li>
     </ul>
     <p class="text-center">If this sounds like you, then feel free to jump in. Click the <a href="demo.php"><b>Logs</b></a>&nbsp;tab to get started.</p><br><br>
-    <div style="display:block;margin-left:auto;margin-right:auto;width:900px;"><b><p class="text-center" style="font-size:1.5em">If you find a bug (problem) or have a suggestion, visit our logs page and post a new workout with your bug report or suggestion</p></b></div>
+    <div class="bugnoteDesktop" id="bugnote"><b><p class="text-center" style="font-size:1.5em">If you find a bug (problem) or have a suggestion, visit our logs page and post a new workout with your bug report or suggestion</p></b></div>
     <a class="btn btn-link" href="https://github.com/Jeak/ggLog">our Github :D</a>
     <p>Powered by <a href="http://twitter.github.io/bootstrap/">#Bootstrap</a></p>
-    <div style="width:100%;"><a href="http://imgur.com/AYamMmn"><img style="display:block;margin-left:auto;margin-right:auto;" src="http://i.imgur.com/AYamMmn.png" title="Hosted by imgur.com" /></a></div>
+    <div style="width:100%;"><a href="http://imgur.com/AYamMmn"><img class="donBoscoDesktop" src="http://i.imgur.com/AYamMmn.png" title="Hosted by imgur.com" id="donbosco" /></a></div>
   </body>
 </html>
